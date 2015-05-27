b13f882bacfbca4e4b48c57b5f37b6ba63e07124
astro8891 <ashley.j.stewart@live.com>
2015-05-27 19:02:11 +1200

    Player can now choose to be hit

--- ConsoleApplication1/GameLogic.cs
+++ ConsoleApplication1/GameLogic.cs
@@ -13,8 +13,15 @@ namespace BlackJackVS
         public string[] Suits { get; set; }
         public Dictionary<string, int> CardDeck { get; set; }
 
+        Player Player = new Player();
+        House Casino = new House();
+
         public Game()
         {
+
+            Player.score = 0;
+            Casino.score = 0;
+
             Suits = new string[] { "hearts", "diamonds", "clubs", "spades" };
 
             CardDeck = new Dictionary<string, int>
@@ -33,13 +40,11 @@ namespace BlackJackVS
                  {"King", 10},
                  {"Ace", 10}
              };
-
-
         }
 
 
 
-        public string DrawCard()
+        public string DrawInitialPlayerCards()
         {
             var rnd = new Random();
 
@@ -48,8 +53,47 @@ namespace BlackJackVS
             string card1 = CardDeck.Keys.ElementAt(rnd.Next(CardDeck.Count - 1));
             string card2 = CardDeck.Keys.ElementAt(rnd.Next(CardDeck.Count - 1));
 
-            return ("The Draw is " + card1 + " of " + Suits[suitIndex1] + " and " + card2 + " of " + Suits[suitIndex2] + " thats a scoore of " + (CardDeck[card1] + CardDeck[card2]));
-            
+            Player.score = (CardDeck[card1] + CardDeck[card2]);
+
+            return ("Your Draw is " + card1 + " of " + Suits[suitIndex1] + " and " + card2 + " of " + Suits[suitIndex2] + " thats a scoore of " + Player.score);
+
+        }
+
+        public string DrawInitialHouseCards()
+        {
+            var rnd = new Random();
+
+            int suitIndex1 = rnd.Next(Suits.Length);
+            int suitIndex2 = rnd.Next(Suits.Length);
+            string card1 = CardDeck.Keys.ElementAt(rnd.Next(CardDeck.Count - 1));
+            string card2 = CardDeck.Keys.ElementAt(rnd.Next(CardDeck.Count - 1));
+
+            Casino.score = (CardDeck[card1] + CardDeck[card2]);
+
+            return ("The Casino Draw is " + card1 + " of " + Suits[suitIndex1] + " and " + card2 + " of " + Suits[suitIndex2] + " thats a scoore of " + Casino.score);
+
+        }
+
+        public void Hit(string UserAnswer)
+        {
+            if (UserAnswer == "yes" || UserAnswer == "Yes" || UserAnswer == "YES")
+            {
+                dealAdditionalUser();
+            }
+            else
+            {
+                
+            }
+
+        }
+
+        public void dealAdditionalUser()
+        {
+            Random rnd = new Random();
+            int suitIndex1 = rnd.Next(Suits.Length);
+            string card1 = CardDeck.Keys.ElementAt(rnd.Next(CardDeck.Count - 1));
+            Console.WriteLine("Your draw is" + Suits[suitIndex1] + " of " + card1);
+            Console.WriteLine(Player.score = (Player.score + CardDeck[card1]));
         }
     }
 }
