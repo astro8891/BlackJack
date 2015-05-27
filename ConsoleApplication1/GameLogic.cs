6fd5f86463618fafcef3e18bbbb1b58fe414342b
astro8891 <ashley.j.stewart@live.com>
2015-05-27 18:17:02 +1200

    redo in VS13 after 15 crashed

---
+++ ConsoleApplication1/GameLogic.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackVS
+{
+
+    public class Game
+    {
+
+        public string[] Suits { get; set; }
+        public Dictionary<string, int> CardDeck { get; set; }
+
+        public Game()
+        {
+            Suits = new string[] { "hearts", "diamonds", "clubs", "spades" };
+
+            CardDeck = new Dictionary<string, int>
+             {
+                 {"Two", 2},
+                 {"Three", 3},
+                 {"Four", 4},
+                 {"Five", 5},
+                 {"Six", 6},
+                 {"Seven", 7},
+                 {"Eight", 8},
+                 {"Nine", 9},
+                 {"Ten", 10},
+                 {"Jack", 10},
+                 {"Queen", 10},
+                 {"King", 10},
+                 {"Ace", 10}
+             };
+
+
+        }
+
+
+
+        public string DrawCard()
+        {
+            var rnd = new Random();
+
+            int suitIndex1 = rnd.Next(Suits.Length);
+            int suitIndex2 = rnd.Next(Suits.Length);
+            string card1 = CardDeck.Keys.ElementAt(rnd.Next(CardDeck.Count - 1));
+            string card2 = CardDeck.Keys.ElementAt(rnd.Next(CardDeck.Count - 1));
+
+            return ("The Draw is " + card1 + " of " + Suits[suitIndex1] + " and " + card2 + " of " + Suits[suitIndex2] + " thats a scoore of " + (CardDeck[card1] + CardDeck[card2]));
+            
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            Console.WriteLine(game.DrawCard());
+            Console.ReadLine();
+        }
+    }
+}
